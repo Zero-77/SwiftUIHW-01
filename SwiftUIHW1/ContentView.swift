35f59106543663d3b777ba77a043b372b6256728
Zero-77 <07072u4bp6@gmail.com>
2020-03-14 17:17:35 +0800

    Initial Commit

---
+++ SwiftUIHW1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIHW1
+//
+//  Created by 07 Zhang on 2020/3/14.
+//  Copyright © 2020 07 Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
